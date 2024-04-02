f8efb03a488721c7dc31531fa11ce07797790e6e
ManhThanhz <100328483+ManhThanhz@users.noreply.github.com>
2024-04-02 23:48:58 +0700

    feat: Navigation Bar
    feat: Navigation Bar

--- WebApplication/yolohome-client/src/App.js
+++ WebApplication/yolohome-client/src/App.js
@@ -1,18 +1,44 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Remove Switch
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'; // Remove Switch
 import Login from './pages/Login';
 import Register from './pages/Register'; // Update import
 import Dashboard from './pages/Dashboard';
+import NavBar from './pages/NavBar';
 // import DefaultLayout from './components/layouts/DefaultLayout';
+const MaybeShowNavbar = ({children}) => {
+  const location = useLocation();
+  const [showNavbar, setShowNavbar] = useState(false);
+
+  useEffect(() => {
+    if (location.pathname === '/' || location.pathname === '/register' || location.pathname === '/PrintConfig') {
+      setShowNavbar(false);
+    }
+    else {
+      setShowNavbar(true);
+    }
+  }, [location])
+
+  return (
+    <div>{showNavbar && children}</div>
+  )
+}
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Login></Login>} />
-        <Route path="/register" element={<Register></Register>} /> {/* Update Route */}
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+      <div className="App">
+        <MaybeShowNavbar>
+          <NavBar/>
+        </MaybeShowNavbar>
+        <div className='content'>
+          <Routes>
+            <Route path="/" element={<Login></Login>} />
+            <Route path="/register" element={<Register></Register>} /> {/* Update Route */}
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </div>
+      </div>
     </Router>
   );
 }
