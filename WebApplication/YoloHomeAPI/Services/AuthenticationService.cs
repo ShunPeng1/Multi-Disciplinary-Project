8f9b0a4bf2ec19176d77cf163abdb91ea2e090df
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2024-04-03 13:10:59 +0700

    feat: username and password checking

--- WebApplication/YoloHomeAPI/Services/AuthenticationService.cs
+++ WebApplication/YoloHomeAPI/Services/AuthenticationService.cs
@@ -4,6 +4,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
+using System.Text.RegularExpressions;
 using Microsoft.IdentityModel.Tokens;
 using YoloHomeAPI;
 using YoloHomeAPI.Contexts;
@@ -26,15 +27,37 @@ namespace YoloHomeAPI.Services
             _authenticationSettings = authenticationSettings;
         }
 
+        
         public IAuthenticationService.AuthenticationResult Register(string username, string password)
         {
-            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+            if (CheckNull(username) || CheckNull(password))
             {
                 return new(false, "Username or password cannot be empty.", "");
             }
+            
+            if (CheckIllegalCharacterInUserName(username))
+            {
+                return new(false, "Username contains illegal characters.", "");
+            }
+            
+            if (CheckCharacterInPassword(password))
+            {
+                return new(false, "Password must contain at least one uppercase letter, one lowercase letter, one number, one special character and be at least 8 characters long.", "");
+            }
+            
+            if (CheckLength(username, 3, 20))
+            {
+                return new(false, "Username must be between 3 and 20 characters long.", "");
+            }
+            
+            if (CheckLength(password, 8, 20))
+            {
+                return new(false, "Password must be between 8 and 20 characters long.", "");
+            }
+            
 
             /*
-            if (_context.Users.Any(x => x.UserName == username))
+            if (_context.Users.Any(x => x.UserName == userName))
             {
                 return new(false, "Username already exists.", "");
             }
@@ -58,13 +81,33 @@ namespace YoloHomeAPI.Services
 
         public IAuthenticationService.AuthenticationResult Login(string username, string password)
         {
-            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+            if (CheckNull(username) || CheckNull(password))
             {
                 return new(false, "Username or password cannot be empty.", "");
             }
+            
+            if (CheckIllegalCharacterInUserName(username))
+            {
+                return new(false, "Username contains illegal characters.", "");
+            }
+            
+            if (CheckCharacterInPassword(password))
+            {
+                return new(false, "Password must contain at least one uppercase letter, one lowercase letter, one number, one special character and be at least 8 characters long.", "");
+            }
+            
+            if (CheckLength(username, 3, 20))
+            {
+                return new(false, "Username must be between 3 and 20 characters long.", "");
+            }
+            
+            if (CheckLength(password, 8, 20))
+            {
+                return new(false, "Password must be between 8 and 20 characters long.", "");
+            }
 
             /*
-            var user = _context.Users.SingleOrDefault(x => x.UserName == username);
+            var user = _context.Users.SingleOrDefault(x => x.UserName == userName);
 
             if (user == null)
             {
@@ -80,7 +123,47 @@ namespace YoloHomeAPI.Services
             */
             return new(true, "Login successful.", GenerateJwtToken("123456789"));
         }
+        
+        private bool CheckNull(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return true;
+            }
+            return false;
+        }
 
+        private bool CheckIllegalCharacterInUserName(string userName)
+        {
+            Regex rgx = new Regex("[^a-zA-Z0-9 -]");
+            if (rgx.IsMatch(userName))
+            {
+                return true;
+            }
+            return false;
+            
+        }
+        
+        private bool CheckCharacterInPassword(string password)
+        {
+            Regex rgx = new Regex("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$");
+            if (rgx.IsMatch(password))
+            {
+                return true;
+            }
+            return false;
+        }
+        
+        private bool CheckLength(string input, int min, int max)
+        {
+            if (input.Length < min || input.Length > max)
+            {
+                return true;
+            }
+            return false;
+        }
+        
+        
         private static (string hash, string salt) HashPassword(string password)
         {
             using var hmac = new HMACSHA512();
