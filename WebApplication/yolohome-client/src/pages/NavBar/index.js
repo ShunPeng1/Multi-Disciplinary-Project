f8da56f1938392407f9faee9965c8fab0482ee55
CUCCUTTHANBI\tango <tangocnam23@gmail.com>
2024-04-04 10:16:20 +0700

    Update index.js

--- WebApplication/yolohome-client/src/pages/NavBar/index.js
+++ WebApplication/yolohome-client/src/pages/NavBar/index.js
@@ -17,7 +17,7 @@ const NavBar = () => {
         <Logo width={160} />
       </div>
       <div className="navbar-content">
-        <Link to="/Dashboard" className={getNavItemClass("/Dashboard")} id="mainFunction">
+        <Link to="/dashboard" className={getNavItemClass("/dashboard")} id="mainFunction">
           <img src="./Images/home.png" alt="Home Icon" />
           <p>Home</p>
         </Link>
