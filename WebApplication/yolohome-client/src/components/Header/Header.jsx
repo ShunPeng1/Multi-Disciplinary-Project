69c35c88a84599fda917618424229e3f1c299228
CUCCUTTHANBI\tango <tangocnam23@gmail.com>
2024-04-06 15:00:42 +0700

    update front end design (haven completed)

--- WebApplication/yolohome-client/src/components/Header/Header.jsx
+++ WebApplication/yolohome-client/src/components/Header/Header.jsx
@@ -4,7 +4,15 @@ const Header = () => {
 
     return (
         <div className="header">
+          <div className="viewItem">
+            <p className="specialWelcome">Welcome</p>
+            <p className="controlSentence">Control your home from here!</p>
+          </div>
 
+          <div className="viewImg2">
+            <img src="./Images/background_new.png" alt="background_new" ></img>
+          </div>
+          
         </div>
     )
 }
--- WebApplication/yolohome-client/src/pages/Dashboard/index.jsx
+++ WebApplication/yolohome-client/src/pages/Dashboard/index.jsx
@@ -5,47 +5,40 @@ import FanCard from "../../components/Cards/FanCard";
 import DoorCard from "../../components/Cards/DoorCard";
 import HumidityCard from "../../components/Cards/HumidityCard";
 import TemperatureCard from "../../components/Cards/TemperatureCard";
+import Header from "../../components/Header/Header";
 const Dashboard = (props) => {
 
   return (
     <div className="home">
-        <div className="contentSection">
-          <div className="importantView">
-            <div className="viewItem">
-              <p className="specialWelcome">Welcome</p>
-              <p className="controlSentence">Control your home from here!</p>
-            </div>
-
-            <div className="viewImg2">
-              <img src="./Images/background_new.png" alt="background_new" ></img>
-            </div>
-            
+      <div className="contentSection">
+        {/* {Import Header} */}
+        <div className="import-header">
+          <Header />
+        </div>
+        
+        {/* Display Information */}
+        <div className="light-and-humid">
+          <div className="lightCard">
+            <LightCard />
           </div>
-          
-          
-          {/* Display Information */}
-          <div className="light-and-humid">
-            <div className="lightCard">
-              <LightCard />
-            </div>
-            <div className="humidityCard">
-              <HumidityCard />
-            </div>
+          <div className="humidityCard">
+            <HumidityCard />
           </div>
-          
-          <div className="door-and-temp">
-            <div className="doorCard">
-              <DoorCard />
-            </div>
-            <div className="temperatureCard">
-              <TemperatureCard />
-            </div>
+        </div>
+        
+        <div className="door-and-temp">
+          <div className="doorCard">
+            <DoorCard />
           </div>
-          <div className="fanCard">
-            <FanCard />
+          <div className="temperatureCard">
+            <TemperatureCard />
           </div>
-          
-        </div>     
+        </div>
+        <div className="fanCard">
+          <FanCard />
+        </div>
+        
+      </div>     
     </div>
   );
 };
