b0230e96ebe8d30243941b51eb6bac5f8da1b6e4
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2024-04-07 15:24:52 +0700

    feat: humidity and temperature update

--- WebApplication/yolohome-client/src/components/Cards/TemperatureCard/index.jsx
+++ WebApplication/yolohome-client/src/components/Cards/TemperatureCard/index.jsx
@@ -1,68 +1,66 @@
-    import React, { useState, useEffect } from "react";
-    import './TemperatureCard.css';
+import React, { useState, useEffect } from "react";
+import './TemperatureCard.css';
+import FetchRequest from "../../api/api";
 
-    const TemperatureCard = () => {
-        const [temperature, setTemperature] = useState(null);
-        const [errorMessage, setErrorMessage] = useState(null);
-        const [showPopup, setShowPopup] = useState(false);
+const TemperatureCard = () => {
+    const [temperature, setTemperature] = useState(null);
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [showPopup, setShowPopup] = useState(false);
 
-        useEffect(() => {
-            const fetchTemperature = async () => {
-                const temperatureData = await fetchTemperatureData();
-                setTemperature(temperatureData.temperature);
-                if (temperatureData.temperature > 35) {
-                    setErrorMessage(temperatureData.message);
-                    setShowPopup(true);
-                } else {
-                    setShowPopup(false);
-                }
-            };
-            fetchTemperature();
-        }, []);
+    useEffect(() => {
+        fetchData();
+        const intervalId = setInterval(fetchData, 5000); // Fetch data every 5 seconds
 
-        // const fetchTemperatureData = async () => {
-        //     try {
-        //         const response = await fetch('your_api_endpoint');
-        //         if (!response.ok) {
-        //             throw new Error('Failed to fetch temperature data');
-        //         }
-        //         const data = await response.json();
-        //         return data;
-        //     } catch (error) {
-        //         console.error('Error fetching temperature data:', error);
-        //         return { temperature: null, message: 'Error fetching temperature data' };
-        //     }
-        // };
-
-        // example fetchTemperatureData function, replace by the above function or recreate one
-        const fetchTemperatureData = async () => {
-            return { temperature: 45, message: "Temperature is too high!" };
+        return () => {
+            clearInterval(intervalId); // Clear interval on component unmount
         };
+    }, []);
 
-        const handleDismiss = () => {
+    const fetchData = () => {
+        FetchRequest('api/IotDeviceApi/GetLatestSensorData', 'GET', {
+            DeviceType: 'Temper'
+        }, successCallback, errorCallback);
+    };
+
+    const successCallback = (data) => {
+        console.log('Success:', data);
+        setTemperature(data.Response);
+        if (data.Response > 35) {
+            setErrorMessage("Temperature is too high!");
+            setShowPopup(true);
+        } else {
             setShowPopup(false);
-        };
-
-        return (
-            <div className="temperature-container">
-                {showPopup && (
-                    <div className="popup">
-                        <p>🛈 Temperature Alert!</p>
-                        <p>{errorMessage}</p>
-                        <button onClick={handleDismiss}>I know</button>
-                    </div>
-                )}
-                <div className="icon-and-temp">
-                    <img src="./Images/temperature.png" alt="Temperature Icon" />
-                    <div className="temperature-info">
-                        {temperature !== null ? `${temperature}°C` : 'Loading...'}
-                    </div>
-                </div>
-                <div className="temperature-text">
-                    {'Temperature'}
-                </div>
-            </div>
-        );
+        }
     }
 
-    export default TemperatureCard;
+    const errorCallback = (error) => {
+        console.error('Error:', error);
+    }
+
+    const handleDismiss = () => {
+        setShowPopup(false);
+    };
+
+    return (
+      <div className="temperature-container">
+          {showPopup && (
+            <div className="popup">
+                <p>🛈 Temperature Alert!</p>
+                <p>{errorMessage}</p>
+                <button onClick={handleDismiss}>I know</button>
+            </div>
+          )}
+          <div className="icon-and-temp">
+              <img src="./Images/temperature.png" alt="Temperature Icon" />
+              <div className="temperature-info">
+                  {temperature !== null ? `${temperature}°C` : 'Loading...'}
+              </div>
+          </div>
+          <div className="temperature-text">
+              {'Temperature'}
+          </div>
+      </div>
+    );
+}
+
+export default TemperatureCard;
