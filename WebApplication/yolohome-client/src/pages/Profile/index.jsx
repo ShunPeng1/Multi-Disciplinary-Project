556b124020f29a3a39d5ded84a4c3e2fd0549d90
ManhThanhz <100328483+ManhThanhz@users.noreply.github.com>
2024-04-30 23:59:11 +0700

    feat: Profile Page
    feat: Profile Page

--- WebApplication/yolohome-client/src/pages/Profile/index.jsx
+++ WebApplication/yolohome-client/src/pages/Profile/index.jsx
@@ -3,17 +3,62 @@ import "./Profile.css";
 import Header from "../../components/Header/Header";
 
 const Profile = () => {
-
-    return (
-        <div className = "profile">
-            <div className="contentSection2">
-                <div className = "import-header2">
-                    <Header />
-                </div>
-                
-            </div>
+  return (
+    <div className="profile">
+      <div className="contentSection2">
+        <div className="import-header2">
+          <Header />
         </div>
-    );
-}
+
+        <div className="contentSection">
+          <div className="welcome">
+            <p className="specialWelcomee">Hồ sơ của bạn</p>
+          </div>
+          <div className="profilee">
+            <div className="info">
+              <div className="accountProp">
+                <div className="column">
+                  {/* first column */}
+                  <p>First Name</p>
+                  <input
+                    type="text"
+                    disabled="disabled"
+                    value="Thanh"
+                    className="disableInput"
+                  />
+                  <p>Last Name</p>
+                  <input
+                    type="text"
+                    disabled="disabled"
+                    value="Thanh"
+                    className="disableInput"
+                  />
+                  
+                </div>
+                <div className="column">
+                  {/* second column */}
+                  <p>Email</p>
+                  <input
+                    type="text"
+                    disabled="disabled"
+                    value="test@hcmut.edu.vn"
+                    className="disableInput"
+                  />
+                  <p>Username</p>
+                  <input
+                    type="text"
+                    disabled="disabled"
+                    value="Thanh"
+                    className="disableInput"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Profile;
