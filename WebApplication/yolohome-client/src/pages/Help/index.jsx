027f8d9df77afb17bd2fad6fd0387522e949ea0e
ManhThanhz <100328483+ManhThanhz@users.noreply.github.com>
2024-04-30 14:31:37 +0700

    feat: help page
    feat: help page

--- WebApplication/yolohome-client/src/pages/Help/index.jsx
+++ WebApplication/yolohome-client/src/pages/Help/index.jsx
@@ -1,24 +1,69 @@
-import "./Help.css";
-import React from 'react';
+// help.jsx
 
+import "./Help.css";
+import React, { useState } from 'react';
 import Header from "../../components/Header/Header";
+
 const Help = (props) => {
+  const [expanded, setExpanded] = useState(Array(5).fill(false));
+
+  const toggleExpansion = (index) => {
+    const newExpanded = [...expanded];
+    newExpanded[index] = !newExpanded[index];
+    setExpanded(newExpanded);
+  };
+
+  const faqData = [
+    {
+      question: "How do I add a new device to my smart home network?",
+      answer: "To add a new device, first ensure that it's compatible with your smart home system. \
+              In our app, navigate to the Home Page or Rooms section, select Add Device, \
+              and follow the prompts to pair the new device with your account."
+    },
+    {
+      question: "Why is my smart device not responding or connecting to the app?",
+      answer: "If you forget your password, you can..."
+    },
+    {
+      question: "What should I do if I forget my account password?",
+      answer: "You can contact support by..."
+    },
+    {
+      question: "How can I view my activity history in the app?",
+      answer: "Yes, we have a mobile app available for..."
+    },
+    {
+      question: "Can I access my smart home devices remotely when I'm away from home?",
+      answer: "To update your profile, go to..."
+    }
+  ];
 
   return (
     <div className="help">
       <div className="contentSection">
-        {/* {Import Header} */}
-        <div className="import-header">
-          <Header />
+        <div className="faqSection">
+          <h2>Frequently Asked Questions</h2>
+          <div className="faqq">
+          {faqData.map((faq, index) => (
+            <div key={index} className={`faq ${expanded[index] ? 'expanded' : ''}`}>
+              <div className="question">
+                <h3>{faq.question}</h3>
+                <button className="expandButton" onClick={() => toggleExpansion(index)}>+</button>
+              </div>
+              {expanded[index] && (
+                <div className="answer">
+                  <p>{faq.answer}</p>
+                </div>
+              )}
+            </div>
+          ))}
+          </div>
         </div>
-        <a href="https://forms.gle/sZ3vMUUJJxgo1QLc8" target="_blank" rel="noopener noreferrer"> {/* Add this line */}
+        <a href="https://forms.gle/sZ3vMUUJJxgo1QLc8" target="_blank" rel="noopener noreferrer">
           <button className="helpButton">Need help</button>
-        </a> {/* Add this line */}
+        </a>  
       </div>
-
-
-    </div>  
-   
+    </div>
   );
 };
 
